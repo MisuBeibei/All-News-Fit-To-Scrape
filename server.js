6af81981951f07572d282fe4cd6fef944ccb61c3
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-08-10 22:46:44 -0700

    get and post routes for finding and updating data from mongoDB scraped

--- server.js
+++ server.js
@@ -49,19 +49,101 @@ request("http://www.nytimes.com/trending/", function(error, response, html) {
     var title = $div.children('h4').text();
     // Save summary
     var summary = $div.children('p').text();
-    // Create mongoose model
+
     var scrapedData = new ScrapedData({
       title: title,
       imgURL: imgURL,
       summary: summary,
       articleURL: articleURL
     });
-    // Save data
     scrapedData.save(function(err) {
       if (err) {
-        //console.log(err);
+        return err
       }
-      //console.log('Saved');
     });
   });
+});
+
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  ScrapedData
+    .findOne()
+    .exec(function(err,data) {
+      if (err) return console.error(err);
+      res.render('index', {
+        imgURL: data.imgURL,
+        title: data.title,
+        summary: data.summary,
+        _id: data._id,
+        articleURL: data.articleURL,
+        comments: data.comments
+      });
+    })
+});
+
+app.get('/next/:id', function(req, res) {
+  ScrapedData
+    .find({
+      _id: {$gt: req.params.id}
+    })
+    .sort({_id: 1 })
+    .limit(1)
+    .exec(function(err,data) {
+      if (err) return console.error(err);
+      res.json(data);
+    })
+});
+
+app.get('/prev/:id', function(req, res) {
+  ScrapedData
+    .find({
+      _id: {$lt: req.params.id}
+    })
+    .sort({_id: -1 })
+    .limit(1)
+    .exec(function(err,data) {
+      if (err) return console.error(err);
+      res.json(data);
+    })
+});
+
+app.post('/comment/:id', function(req, res) {
+  ScrapedData.findByIdAndUpdate(
+    req.params.id,
+    {$push: {
+      comments: {
+        text: req.body.comment
+      }
+    }},
+    {upsert: true, new: true},
+    function(err, data) {
+      if (err) return console.error(err);
+      res.json(data.comments);
+    }
+  );
+});
+
+app.post('/remove/:id', function(req, res) {
+  ScrapedData.findByIdAndUpdate(
+    req.params.id,
+    {$pull: {
+      comments: {
+        _id: req.body.id
+      }
+    }},
+    {new: true},
+    function(err, data) {
+      if (err) return console.error(err);
+      res.json(data.comments);
+    }
+  );
+});
+
+app.listen(3000, function() {
+  console.log('PORT 3000 is listening!');
 });
